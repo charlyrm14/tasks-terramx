3d1b4e4ea8e2b8a64753842ccac3b1ad6a18ea34
charlyrm14 <charlyrm14@gmail.com>
2025-04-17 21:21:52 -0600

    feature: se agrega funcionalidad de edicion de tareas

--- app/Controllers/TaskController.php
+++ app/Controllers/TaskController.php
@@ -20,12 +20,18 @@ class TaskController {
         require VIEWS_PATH . '/home.php';
     }
     
+    /**
+     * Vista formulario creación de tarea
+     */
     public function add()
     {
         $error = [];
         require VIEWS_PATH . '/add.php';
     }
 
+    /**
+     * Lógica creación de tarea 
+     */
     public function create()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') { // Valida que la petición se haya enviado a través del método POST
@@ -58,6 +64,80 @@ class TaskController {
             header("Location: /");
             exit;  
 
+        } else {
+
+            /**
+             * Si se accede directamente por URL reedirige a página de inicio
+             */
+
+            header("Location: /");
+            exit;
+        }
+    }
+
+    /**
+     * Vista edición de tarea
+     */
+    public function edit(int $id)
+    {
+        $model = new Task();
+        $task = $model->getTaskById($id);
+
+        if (!$task) {
+            require VIEWS_PATH . '/404.php';
+            return;
+        }
+
+        require VIEWS_PATH . '/edit.php';
+    }
+
+    /**
+     * Lógica actualización de tarea
+     */
+    public function update(int $id)
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') { // Valida que la petición se haya enviado a través del método POST
+
+            $model = new Task();
+            $task = $model->getTaskById($id);
+
+            if (!$task) {
+                http_response_code(404);
+                echo "Página no encontrada";
+            }
+
+            $task_name = $_POST['task_name'];
+            $description = $_POST['description'];
+            $status = $_POST['status'];
+            $error = [];
+
+            
+            /**
+             * Validación formulario
+            */
+            if(empty($task_name) || empty($description) || empty($status)) {
+                $error['task_name'] = 'El nombre de la tarea es obligatorio';
+                $error['description'] = 'La descripción de la tarea es obligatorio';
+                $error['status'] = 'Selecciona un estatus para esta tarea';
+                require VIEWS_PATH . '/edit.php';
+            } 
+
+            /**
+             * Crea nueva tarea
+             */
+            try {
+                
+                $update_task = new Task();
+                $update_task->updateTaskById($task['id'], $task_name, $description, $status);
+
+            } catch (Exception $e) {
+                header("Location: /");
+                exit;
+            }
+
+            header("Location: /");
+            exit; 
+            
         } else {
 
             /**
