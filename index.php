6a3996676d1103a9ad0e372dcc754836749352f8
charlyrm14 <charlyrm14@gmail.com>
2025-04-17 20:08:03 -0600

    feature: se agrega vista principal, vista crear tarea y vista 404

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 
 require 'config/database.php';
+
+define('VIEWS_PATH', __DIR__ . '/app/Views');
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+switch ($uri) {
+    case '/':
+        require VIEWS_PATH . '/home.php';
+    break;
+
+    case '/add':
+        require VIEWS_PATH . '/add.php';
+    break;
+
+    default:
+        require VIEWS_PATH . '/404.php';
+    break;
+}
